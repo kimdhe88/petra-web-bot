2602d7a950d1f4771cdab2560280e1b1e15b8bc3
kimdhe88@nate.com <roal109721>
2020-05-22 17:47:50 +0900

    프로젝트 초기화

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark></v-app-bar>
+
+    <v-content>
+      <v-btn>테스트</v-btn>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {},
+
+  data: () => ({
+    //
+  })
+}
+</script>
